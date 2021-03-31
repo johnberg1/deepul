dbc5953a5c038e9a62dc0522a402539028dbff14
Canberk Baykal <canberk.baykal@ug.bilkent.edu.tr>
2021-03-31 14:43:40 +0300

    hw3_helper

--- deepul/hw3_helper.py
+++ deepul/hw3_helper.py
@@ -1,5 +1,24 @@
 from .utils import *
+import numpy as np
+import torch.nn as nn
+import torch.utils.data
+import torchvision
+from torchvision import transforms as transforms
+from .hw4_utils.hw4_models import GoogLeNet
+from PIL import Image as PILImage
+import scipy.ndimage
+import cv2
+import deepul.pytorch_util as ptu
 
+CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+import numpy as np
+import math
+import sys
+
+softmax = None
+model = None
+device = torch.device("cuda:0")
 
 def plot_vae_training_plot(train_losses, test_losses, title, fname):
     elbo_train, recon_train, kl_train = train_losses[:, 0], train_losses[:, 1], train_losses[:, 2]
@@ -74,7 +93,7 @@ def visualize_q1_data(part, dset_id):
     plt.show()
 
 
-def q1_save_results(part, dset_id, fn):
+def q1_a_save_results(part, dset_id, fn):
     train_data, test_data = q1_sample_data(part, dset_id)
     train_losses, test_losses, samples_noise, samples_nonoise = fn(train_data, test_data, part, dset_id)
     print(f'Final -ELBO: {test_losses[-1, 0]:.4f}, Recon Loss: {test_losses[-1, 1]:.4f}, '
@@ -134,26 +153,26 @@ def q2_save_results(part, dset_id, fn):
                  fname=f'results/q2_{part}_dset{dset_id}_interpolations.png')
 
 
-def q3_save_results(dset_id, fn):
-    assert dset_id in [1, 2]
-    data_dir = get_data_dir(3)
-    if dset_id == 1:
-        train_data, test_data = load_pickled_data(join(data_dir, 'svhn.pkl'))
-    else:
-        train_data, test_data = load_pickled_data(join(data_dir, 'cifar10.pkl'))
+# def q3_save_results(dset_id, fn):
+#     assert dset_id in [1, 2]
+#     data_dir = get_data_dir(3)
+#     if dset_id == 1:
+#         train_data, test_data = load_pickled_data(join(data_dir, 'svhn.pkl'))
+#     else:
+#         train_data, test_data = load_pickled_data(join(data_dir, 'cifar10.pkl'))
 
-    vqvae_train_losses, vqvae_test_losses, pixelcnn_train_losses, pixelcnn_test_losses, samples, reconstructions = fn(train_data, test_data, dset_id)
-    samples, reconstructions = samples.astype('float32'), reconstructions.astype('float32')
-    print(f'VQ-VAE Final Test Loss: {vqvae_test_losses[-1]:.4f}')
-    print(f'PixelCNN Prior Final Test Loss: {pixelcnn_test_losses[-1]:.4f}')
-    save_training_plot(vqvae_train_losses, vqvae_test_losses,f'Q3 Dataset {dset_id} VQ-VAE Train Plot',
-                       f'results/q3_dset{dset_id}_vqvae_train_plot.png')
-    save_training_plot(pixelcnn_train_losses, pixelcnn_test_losses,f'Q3 Dataset {dset_id} PixelCNN Prior Train Plot',
-                       f'results/q3_dset{dset_id}_pixelcnn_train_plot.png')
-    show_samples(samples, title=f'Q3 Dataset {dset_id} Samples',
-                 fname=f'results/q3_dset{dset_id}_samples.png')
-    show_samples(reconstructions, title=f'Q3 Dataset {dset_id} Reconstructions',
-                 fname=f'results/q3_dset{dset_id}_reconstructions.png')
+#     vqvae_train_losses, vqvae_test_losses, pixelcnn_train_losses, pixelcnn_test_losses, samples, reconstructions = fn(train_data, test_data, dset_id)
+#     samples, reconstructions = samples.astype('float32'), reconstructions.astype('float32')
+#     print(f'VQ-VAE Final Test Loss: {vqvae_test_losses[-1]:.4f}')
+#     print(f'PixelCNN Prior Final Test Loss: {pixelcnn_test_losses[-1]:.4f}')
+#     save_training_plot(vqvae_train_losses, vqvae_test_losses,f'Q3 Dataset {dset_id} VQ-VAE Train Plot',
+#                        f'results/q3_dset{dset_id}_vqvae_train_plot.png')
+#     save_training_plot(pixelcnn_train_losses, pixelcnn_test_losses,f'Q3 Dataset {dset_id} PixelCNN Prior Train Plot',
+#                        f'results/q3_dset{dset_id}_pixelcnn_train_plot.png')
+#     show_samples(samples, title=f'Q3 Dataset {dset_id} Samples',
+#                  fname=f'results/q3_dset{dset_id}_samples.png')
+#     show_samples(reconstructions, title=f'Q3 Dataset {dset_id} Reconstructions',
+#                  fname=f'results/q3_dset{dset_id}_reconstructions.png')
 
 
 def q4_a_save_results(dset_id, fn):
@@ -193,3 +212,148 @@ def q4_b_save_results(fn):
                  fname=f'results/q4_{part}_samples.png')
     show_samples(reconstructions, title=f'Q4({part}) Reconstructions',
                  fname=f'results/q4_{part}_reconstructions.png')
+    
+###########
+###########
+## GANS
+    
+def plot_gan_training(losses, title, fname):
+    plt.figure()
+    n_itr = len(losses)
+    xs = np.arange(n_itr)
+
+    plt.plot(xs, losses, label='loss')
+    plt.legend()
+    plt.title(title)
+    plt.xlabel('Training Iteration')
+    plt.ylabel('Loss')
+    savefig(fname)
+
+def q1_gan_plot(data, samples, xs, ys, title, fname):
+    plt.figure()
+    plt.hist(samples, bins=50, density=True, alpha=0.7, label='fake')
+    plt.hist(data, bins=50, density=True, alpha=0.7, label='real')
+
+    plt.plot(xs, ys, label='discrim')
+    plt.legend()
+    plt.title(title)
+    savefig(fname)
+    
+def q1_data(n=20000):
+    assert n % 2 == 0
+    gaussian1 = np.random.normal(loc=-1, scale=0.25, size=(n//2,))
+    gaussian2 = np.random.normal(loc=0.5, scale=0.5, size=(n//2,))
+    data = (np.concatenate([gaussian1, gaussian2]) + 1).reshape([-1, 1])
+    scaled_data = (data - np.min(data)) / (np.max(data) - np.min(data) + 1e-8)
+    return 2 * scaled_data -1
+
+def visualize_q1_dataset():
+    data = q1_data()
+    plt.hist(data, bins=50, alpha=0.7, label='train data')
+    plt.legend()
+    plt.show()
+
+
+def q1_b_save_results(part, fn):
+    data = q1_data()
+    losses, samples1, xs1, ys1, samples_end, xs_end, ys_end = fn(data)
+
+    # loss plot
+    plot_gan_training(losses, 'Q1{} Losses'.format(part), 'results/q1{}_losses.png'.format(part))
+
+    # samples
+    q1_gan_plot(data, samples1, xs1, ys1, 'Q1{} Epoch 1'.format(part), 'results/q1{}_epoch1.png'.format(part))
+    q1_gan_plot(data, samples_end, xs_end, ys_end, 'Q1{} Final'.format(part), 'results/q1{}_final.png'.format(part))
+    
+    
+def load_q3_data():
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+    train_data = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+    test_data = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+    return train_data, test_data
+
+def visualize_q3_data():
+    train_data, _ = load_q3_data()
+    imgs = train_data.data[:100]
+    show_samples(imgs.reshape([100, 28, 28, 1]) * 255.0, title=f'MNIST samples')
+
+def plot_q3_supervised(pretrained_losses, random_losses, title, fname):
+    plt.figure()
+    xs = np.arange(len(pretrained_losses))
+    plt.plot(xs, pretrained_losses, label='bigan')
+    xs = np.arange(len(random_losses))
+    plt.plot(xs, random_losses, label='random init')
+    plt.legend()
+    plt.title(title)
+    savefig(fname)
+
+def q3_save_results(fn):
+    train_data, test_data = load_q3_data()
+    gan_losses, samples, reconstructions, pretrained_losses, random_losses = fn(train_data, test_data)
+
+    plot_gan_training(gan_losses, 'Q3 Losses', 'results/q3_gan_losses.png')
+    plot_q3_supervised(pretrained_losses, random_losses, 'Linear classification losses', 'results/q3_supervised_losses.png')
+    show_samples(samples * 255.0, fname='results/q3_samples.png', title='BiGAN generated samples')
+    show_samples(reconstructions * 255.0, nrow=20, fname='results/q3_reconstructions.png', title=f'BiGAN reconstructions')
+    print('BiGAN final linear classification loss:', pretrained_losses[-1])
+    print('Random encoder linear classification loss:', random_losses[-1])
+    
+def get_colored_mnist(data):
+    # from https://www.wouterbulten.nl/blog/tech/getting-started-with-gans-2-colorful-mnist/
+    # Read Lena image
+    lena = PILImage.open('deepul/deepul/hw4_utils/lena.jpg')
+
+    # Resize
+    batch_resized = np.asarray([scipy.ndimage.zoom(image, (2.3, 2.3, 1), order=1) for image in data])
+
+    # Extend to RGB
+    batch_rgb = np.concatenate([batch_resized, batch_resized, batch_resized], axis=3)
+
+    # Make binary
+    batch_binary = (batch_rgb > 0.5)
+
+    batch = np.zeros((data.shape[0], 28, 28, 3))
+
+    for i in range(data.shape[0]):
+        # Take a random crop of the Lena image (background)
+        x_c = np.random.randint(0, lena.size[0] - 64)
+        y_c = np.random.randint(0, lena.size[1] - 64)
+        image = lena.crop((x_c, y_c, x_c + 64, y_c + 64))
+        image = np.asarray(image) / 255.0
+
+        # Invert the colors at the location of the number
+        image[batch_binary[i]] = 1 - image[batch_binary[i]]
+
+        batch[i] = cv2.resize(image, (0, 0), fx=28 / 64, fy=28 / 64, interpolation=cv2.INTER_AREA)
+    return batch.transpose(0, 3, 1, 2)
+
+def load_q4_data():
+    train, _ = load_q3_data()
+    mnist = np.array(train.data.reshape(-1, 28, 28, 1) / 255.0)
+    colored_mnist = get_colored_mnist(mnist)
+    return mnist.transpose(0, 3, 1, 2), colored_mnist
+
+def visualize_cyclegan_datasets():
+    mnist, colored_mnist = load_q4_data()
+    mnist, colored_mnist = mnist[:100], colored_mnist[:100]
+    show_samples(mnist.reshape([100, 28, 28, 1]) * 255.0, title=f'MNIST samples')
+    show_samples(colored_mnist.transpose([0, 2, 3, 1]) * 255.0, title=f'Colored MNIST samples')
+
+def q4_save_results(fn):
+    mnist, cmnist = load_q4_data()
+
+    m1, c1, m2, c2, m3, c3 = fn(mnist, cmnist)
+    m1, m2, m3 = m1.repeat(3, axis=3), m2.repeat(3, axis=3), m3.repeat(3, axis=3)
+    mnist_reconstructions = np.concatenate([m1, c1, m2], axis=0)
+    colored_mnist_reconstructions = np.concatenate([c2, m3, c3], axis=0)
+
+    show_samples(mnist_reconstructions * 255.0, nrow=20,
+                 fname='figures/q4_mnist.png',
+                 title=f'Source domain: MNIST')
+    show_samples(colored_mnist_reconstructions * 255.0, nrow=20,
+                 fname='figures/q4_colored_mnist.png',
+                 title=f'Source domain: Colored MNIST')
+    pass
